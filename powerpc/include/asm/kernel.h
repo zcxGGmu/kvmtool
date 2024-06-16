d67e7af663fd10bce0b654fe36fd41f8bd05a080
zcxGGmu <zhouquan@iscas.ac.cn>
2024-06-16 21:49:41 +0800

    kvmtool-riscv: Retain only the RISC-V architecture
    Signed-off-by: zcxGGmu <zhouquan@iscas.ac.cn>

--- powerpc/include/asm/kernel.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-
-#ifndef __ASM_KERNEL_H
-#define __ASM_KERNEL_H
-
-#define NR_CPUS	2048
-
-#endif /* __ASM_KERNEL_H */
--- x86/include/asm/kernel.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-
-#ifndef _ASM_KERNEL_H
-#define _ASM_KERNEL_H
-
-#define NR_CPUS	8196
-
-#endif /* _ASM_KERNEL_H */
